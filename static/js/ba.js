fdb08a908d9ae0ba1fe3b1283b1b61976878f7fa
xzx <563905150@qq.com>
2021-03-26 16:02:36 +0800

    fix docs detail style bugs

---
+++ static/js/ba.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = "https://hm.baidu.com/hm.js?ab8d86daab9a8e98cf8faa239aefcd3c";
+var s = document.getElementsByTagName("script")[0]; 
+s.parentNode.insertBefore(hm, s);
+})();
