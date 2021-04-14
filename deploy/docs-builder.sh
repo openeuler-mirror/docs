a965ed575d2893056f9a67aac5d9fa6f96549d57
zoujinda <826835384@qq.com>
2021-04-14 09:48:57 +0800

    open russion document script

--- deploy/docs-builder.sh
+++ deploy/docs-builder.sh
@@ -24,10 +24,10 @@ do
   mkdir -p ../website/content/en/docs/$b
   cp -r ./docs/en/* ../website/content/en/docs/$b/ 
   
-  #if [ -d "./docs/ru" ]; then
-    #mkdir -p ../website/content/ru/docs/$b
-        #cp -r ./docs/ru/* ../website/content/ru/docs/$b/ 
-  #fi
+  if [ -d "./docs/ru" ]; then
+    mkdir -p ../website/content/ru/docs/$b
+        cp -r ./docs/ru/* ../website/content/ru/docs/$b/ 
+  fi
   
   cd ..
   cd ./docs
